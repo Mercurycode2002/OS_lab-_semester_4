5e5f03d8dd48e105e7bcfebe8342a957d30cdc45
mercury <60378051+Mercurycode2002@users.noreply.github.com>
2023-02-01 10:59:54 +0500

    Add files via upload

--- lab 02/Q1.sh
+++ lab 02/Q1.sh
@@ -1,12 +1,13 @@
-echo "Num 1: $1"
-echo "Num 2: $2"
-echo "Num 3: $3"
-if [ $1 -lt $3 ] && [ $1 -lt $3 ]
-then
-	echo "$1 is the smallest"
-elif [ $2 -lt $3 ] && [ $2 -lt $1 ]
-then
-	echo "$2 is the smallest"
-else 
-	echo "$3 is the smallest"
-fi
+for i in {1..5}
+do
+	name="test$i"
+	cd $name
+	for j in {1..2}
+	do
+		cd $j
+		cat test.txt >> /home/student/Q3.txt
+		cd ..
+	done
+	cd ..
+done
+
