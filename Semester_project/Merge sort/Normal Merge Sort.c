8722bb47ad40a5ed726f1b2c059e6a305daf51f9
Zaaaaiinnn <123715968+Zaaaaiinnn@users.noreply.github.com>
2023-05-07 16:47:54 -0400

    Add files via upload

---
+++ Semester_project/Merge sort/Normal Merge Sort.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    int L[n1], R[n2];
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+ 
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int s[] = {2, 3, 4, 56, 0, 32, 44, 5};
+    int n = sizeof(s) / sizeof(s[0]);
+
+    clock_t start = clock();
+    mergeSort(s, 0, n - 1);
+    clock_t end = clock();
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC; // in seconds
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", s[i]);
+
+    printf("\nTime taken: %f seconds\n", time_taken);
+
+    return 0;
+}
+
