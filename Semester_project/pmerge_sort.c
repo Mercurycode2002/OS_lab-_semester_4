269c95782aaada093f44e09efd3812bfcc17add7
mercury <60378051+Mercurycode2002@users.noreply.github.com>
2023-05-07 07:14:47 -0400

    Update pmerge_sort.c

--- Semester_project/pmerge_sort.c
+++ Semester_project/pmerge_sort.c
@@ -1,78 +1,73 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/wait.h>
-
-void merge(int arr[], int start, int mid, int end) {
-    int n1 = mid - start + 1;
-    int n2 = end - mid;
-
-    int left[n1], right[n2];
-    for (int i = 0; i < n1; i++) {
-        left[i] = arr[start + i];
-    }
-    for (int i = 0; i < n2; i++) {
-        right[i] = arr[mid + 1 + i];
-    }
-
-    int i = 0, j = 0, k = start;
+#include <sys/shm.h>
+#include <iostream>
+using namespace std;
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1;i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
+        if(L[i] <= R[j]){
+            arr[k] = L[i];
             i++;
-        } else {
-            arr[k] = right[j];
+        }
+        else{
+            arr[k] = R[j];
             j++;
         }
         k++;
     }
-
-    while (i < n1) {
-        arr[k] = left[i];
+    while(i < n1){
+        arr[k] = L[i];
         i++;
         k++;
     }
-
-    while (j < n2) {
-        arr[k] = right[j];
+    while(j < n2){
+        arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void merge_sort(int arr[], int start, int end) {
-    if (start < end) {
-        int mid = (start + end) / 2;
-
-        int pid = fork();
-
-        if (pid == -1) {
+void merge_sort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        pid_t pid = fork();
+        if (pid < 0) {
             perror("fork");
-            exit(1);
-        } else if (pid == 0) {
-            // child process
-            merge_sort(arr, start, mid);
-            exit(0);
-        } else {
-            // parent process
-            merge_sort(arr, mid + 1, end);
-            wait(NULL);
-            merge(arr, start, mid, end);
+            exit(EXIT_FAILURE);
         }
+        else if (pid == 0) {
+            merge_sort(arr, l, m);
+            _exit(EXIT_SUCCESS);
+        }
+        else {
+            merge_sort(arr, m + 1, r);
+            int status;
+            waitpid(pid, &status, 0);
+        }
+        merge(arr, l, m, r);
     }
 }
 
-int main() {
-    int arr[] = {5, 1, 8, 4, 2, 9, 3, 7, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+int main(int argc, char* argv[]){
+    int s[] = {2, 3, 4, 56, 0, 32, 44, 5};
+    merge_sort(s, 0, 7);
 
-    merge_sort(arr, 0, n - 1);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    cout<<"sorting left side: "<<endl;
+    for(int i = 0; i < 8; i++)
+        cout<<s[i]<<", ";
 
     return 0;
 }
